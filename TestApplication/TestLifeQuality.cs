dec6b057408a7730e915fb976f90519064207ae8
Your Name <dani32oo@mail.ru>
2022-06-12 17:34:40 +0300

    Added LifeQualityTest

--- TestApplication/TestLifeQuality.cs
+++ TestApplication/TestLifeQuality.cs
@@ -1,29 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace TestApplication
 {
-    public class TestLifeQuality : ContentPage
+    internal class Client
     {
-        public TestLifeQuality()
+        /// <summary>
+        /// Количество баллов заработанных пациентом за тест
+        /// </summary>
+        public int Score { get; set; }
+        /// <summary>
+        /// Массив ответов
+        /// </summary>
+        public int[] arrayAnswer;
+
+        public Client()
         {
-            Title = "Европейский опросник качества жизнии";
-            Button backButton = new Button
-            {
-                Text = "На главную",
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center
-            };
-            backButton.Clicked += BackButton_Click;
-            Content = backButton;
-        }
-        private async void BackButton_Click(object sender, EventArgs e)
-        {
-            await Navigation.PopAsync();
+            arrayAnswer = new int[6];
         }
     }
+
+    public partial class TestLifeQuality : ContentPage
+    {
+        Client client;
+
+        public TestLifeQuality()
+        {
+            InitializeComponent();
+            Title = "Европейский опросник качества жизнии";
+            client = new Client();
+        }
+
+        private void button_CalculateResult(object sender, System.EventArgs e)
+        {
+            ReadDataPicker();
+            if (AnswersAllQuestions())
+            {
+                CalculateResult();
+                DisplayAlert("Результат", $"Ваше количетво баллов = {client.Score}", "Принять");
+                //SaveJson(client);
+                client.Score = 0;
+            }
+            //пациент ответил не на все вопросы
+            else
+            {
+                DisplayAlert("Предупреждение", "Пожалуйста ответьте на все вопросы", "Хорошо");
+            }
+        }
+
+        /// <summary>
+        /// Считывание ответов из выпадающих списков
+        /// </summary>
+        private void ReadDataPicker()
+        {
+            client.arrayAnswer[0] = firstQuestion.SelectedIndex;
+            client.arrayAnswer[1] = secondQuestion.SelectedIndex;
+            client.arrayAnswer[2] = thirdQuestion.SelectedIndex;
+            client.arrayAnswer[3] = fourthQuestion.SelectedIndex;
+            client.arrayAnswer[4] = fifthQuestion.SelectedIndex;
+            client.arrayAnswer[5] = sixthQuestion.SelectedIndex;
+        }
+
+        /// <summary>
+        /// Проверка на все ли вопросы ответил пациент
+        /// </summary>
+        /// <returns></returns>
+        private bool AnswersAllQuestions()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (client.arrayAnswer[i] == -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Определение количества баллов за тест
+        /// </summary>
+        private void CalculateResult()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                client.Score += client.arrayAnswer[i];
+            }
+           
+        }
+
+        private void SaveJson(Client _client)
+        {
+            var jsonString = JsonConvert.SerializeObject(_client);
+
+            var fileName = "result.json";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // Documents folder
+            var path = Path.Combine(documentsPath, fileName);
+
+            Console.WriteLine(path);
+            Console.WriteLine(jsonString);
+
+            StreamWriter sw = new StreamWriter(path);
+            sw.WriteLine(jsonString);
+            sw.Close();
+        }
+
+    }
+
 }
