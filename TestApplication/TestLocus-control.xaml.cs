5e44c8e381fa11b8081093d968bf076492c7e149
kerikir <vlasov-kerik@mail.ru>
2022-06-14 17:03:56 +0300

    Добавление взаимодествия локуса-контроля

--- TestApplication/TestLocus-control.xaml.cs
+++ TestApplication/TestLocus-control.xaml.cs
@@ -9,12 +9,104 @@ using Xamarin.Forms.Xaml;
 
 namespace TestApplication
 {
+    public class ClientKirill
+    {
+        /// <summary>
+        /// Количество баллов заработанных пациентом за тест
+        /// </summary>
+        public int Score { get; set; }
+        /// <summary>
+        /// Массив ответов
+        /// </summary>
+        public int[] arrayAnswer;
+
+        public ClientKirill()
+        {
+            arrayAnswer = new int[9];
+        }
+    }
+
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TestLocus_control : ContentPage
     {
+        ClientKirill client;
+
         public TestLocus_control()
         {
             InitializeComponent();
+            client = new ClientKirill();
+        }
+
+
+        /// <summary>
+        /// Получение результатов теста
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_CalculateResult(object sender, System.EventArgs e)
+        {
+            ReadDataPicker();
+            if (AnswersAllQuestions())
+            {
+                CalculateResult();
+                DisplayAlert("Результат", $"Ваше количетво баллов = {client.Score}", "Принял");
+
+                client.Score = 0;
+            }
+            //пациент ответил не на все вопросы
+            else
+            {
+                DisplayAlert("Предупреждение", "Пожалуйста ответьте на все вопросы", "Хорошо");
+            }
+        }
+
+        /// <summary>
+        /// Считывание ответов из выпадающих списков
+        /// </summary>
+        private void ReadDataPicker()
+        {
+            client.arrayAnswer[0] = pickerFirstQuestion.SelectedIndex;
+            client.arrayAnswer[1] = pickerSecondQuestion.SelectedIndex;
+            client.arrayAnswer[2] = pickerThirdQuestion.SelectedIndex;
+            client.arrayAnswer[3] = pickerFourthQuestion.SelectedIndex;
+            client.arrayAnswer[4] = pickerFifthQuestion.SelectedIndex;
+            client.arrayAnswer[5] = pickerSixthQuestion.SelectedIndex;
+            client.arrayAnswer[6] = pickerSeventhQuestion.SelectedIndex;
+            client.arrayAnswer[7] = pickerEighthQuestion.SelectedIndex;
+            client.arrayAnswer[8] = pickerNinthQuestion.SelectedIndex;
+        }
+
+        /// <summary>
+        /// Проверка на все ли вопросы ответил пациент
+        /// </summary>
+        /// <returns></returns>
+        private bool AnswersAllQuestions()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (client.arrayAnswer[i] == -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Определение количества баллов за тест
+        /// </summary>
+        private void CalculateResult()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                client.Score += (4 - client.arrayAnswer[i]);
+            }
+            for (int i = 5; i < 9; i++)
+            {
+                client.Score += client.arrayAnswer[i];
+            }
         }
     }
 }
